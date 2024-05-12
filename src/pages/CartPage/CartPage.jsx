209aa2efb40a91ac718e55925e4b3b38e7c52926
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-05-11 18:16:37 -0700

    feat: Style CartPage

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,19 +1,36 @@
 import { useOutletContext } from "react-router-dom";
 import ShopItem from "../../components/ShopItem/ShopItem";
+import styles from "./CartPage.module.css";
 
 const CartPage = () => {
   const { itemsData, setItemsData } = useOutletContext();
   const itemsInCart = itemsData
     ? itemsData.filter((item) => item.quantity !== 0)
-    : null;
+    : [];
+  const subtotal = itemsInCart?.reduce((subtotal, item) => {
+    return subtotal + item.quantity * item.price;
+  }, 0);
 
   return (
-    <main aria-label="cart page">
-      <p>Your Cart</p>
-      {itemsInCart &&
-        itemsInCart.map((item) => (
-          <ShopItem key={item.id} item={item} setItemsData={setItemsData} />
-        ))}
+    <main aria-label="cart page" className={styles.cartPageContainer}>
+      <p className={styles.title}>Your Cart</p>
+      {itemsInCart.length === 0 ? (
+        <p className={styles.noItemsMessage}>No items in cart</p>
+      ) : (
+        <>
+          <div className={styles.cardsContainer}>
+            {itemsInCart &&
+              itemsInCart.map((item) => (
+                <ShopItem
+                  key={item.id}
+                  item={item}
+                  setItemsData={setItemsData}
+                />
+              ))}
+          </div>
+          <p className={styles.subtotal}>Subtotal: ${subtotal}</p>
+        </>
+      )}
     </main>
   );
 };
